3c35e361379344a2304c9d902a9dc477e093d47d
Gustavo Secretaria <gs98134@gmail.com>
2023-12-20 16:47:40 -0300

    Adicionando capacidade de rearanjo

--- script.js
+++ script.js
@@ -1,62 +1,44 @@
-// Carregar livros do localStorage ao carregar a página
 window.onload = function () {
     var bookshelf = document.getElementById('bookshelf');
     var books = JSON.parse(localStorage.getItem('books')) || [];
     books.forEach(function (book, index) {
         addBookToShelf(book.materia, book.link, index);
     });
+
+    new Sortable(bookshelf, {
+        animation: 150,
+        chosenClass: "sortable-chosen",
+        dragClass: "sortable-drag",
+    });
 };
 
-// Adicionar novo livro ao enviar o formulário
 document.getElementById('addBookForm').addEventListener('submit', function (event) {
     event.preventDefault();
     var materia = document.getElementById('materia').value;
     var link = document.getElementById('link').value;
 
-    // Salvar novo livro no localStorage
     var books = JSON.parse(localStorage.getItem('books')) || [];
     books.push({ materia: materia, link: link });
     localStorage.setItem('books', JSON.stringify(books));
 
     addBookToShelf(materia, link, books.length - 1);
+
+    document.getElementById('materia').value = '';
+    document.getElementById('link').value = '';
 });
 
-// function addBookToShelf(materia, link) {
-//     var bookshelf = document.getElementById('bookshelf');
-//     var book = document.createElement('div');
-//     book.className = 'book';
-//     var linkElement = document.createElement('a');
-//     linkElement.href = link;
-//     linkElement.target = '_blank';
-//     linkElement.textContent = materia;
-//     var remove = document.createElement('div');
-//     remove.className = 'remove';
-//     remove.textContent = 'X';
-//     remove.addEventListener('click', function (event) {
-//         event.stopPropagation();
-//         var books = JSON.parse(localStorage.getItem('books'));
-//         var bookIndex = books.findIndex(function (book) {
-//             return book.materia === materia && book.link === link;
-//         });
-//         if (bookIndex !== -1) {
-//             books.splice(bookIndex, 1);
-//             localStorage.setItem('books', JSON.stringify(books));
-//         }
-//         bookshelf.removeChild(book);
-//     });
-//     book.appendChild(linkElement);
-//     book.appendChild(remove);
-//     bookshelf.appendChild(book);
-// }
-
-function addBookToShelf(materia, link) {
+function addBookToShelf(materia, link, index) {
     var bookshelf = document.getElementById('bookshelf');
     var book = document.createElement('div');
     book.className = 'book';
+    book.draggable = true;
+    book.id = 'book-' + index;
+
     var linkElement = document.createElement('a');
     linkElement.href = link;
     linkElement.target = '_blank';
     linkElement.textContent = materia;
+
     var remove = document.createElement('div');
     remove.className = 'remove';
     remove.textContent = 'X';
@@ -75,13 +57,8 @@ function addBookToShelf(materia, link) {
             bookshelf.removeChild(book);
         }
     });
+
     book.appendChild(linkElement);
     book.appendChild(remove);
     bookshelf.appendChild(book);
-
-    document.getElementById('materia').value = '';
-    document.getElementById('link').value = '';
 }
-
-
-
